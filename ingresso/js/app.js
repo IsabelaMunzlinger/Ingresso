24c7ff5c4d47b895227e1fce80aab79a8f295ec4
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2024-02-16 17:28:05 -0300

    Adicionando arquivos da pasta ingresso

---
+++ ingresso/js/app.js
@@ -0,0 +1,37 @@
+function comprar(){
+    let tipoDeIngresso = document.getElementById('tipo-ingresso').value;
+    let quantidade = parseInt(document.getElementById('qtd').value);
+    let pistaElement = document.getElementById('qtd-pista');
+    let cadeiraSuperiorElement = document.getElementById('qtd-superior');
+    let cadeiraInferiorElement = document.getElementById('qtd-inferior');
+
+    let pista = parseInt(pistaElement.textContent);
+    let cadeiraSuperior = parseInt(cadeiraSuperiorElement.textContent);
+    let cadeiraInferior = parseInt(cadeiraInferiorElement.textContent);
+
+    if(quantidade < 0){
+        alert('Adicione a quantidade positiva e diferente de zero.');
+        document.getElementById('qtd').value = '';
+        return;
+    }
+
+    if(tipoDeIngresso == "pista" && quantidade <= pista && quantidade > 0){
+        pista -= quantidade;
+        pistaElement.textContent = pista;
+        document.getElementById('qtd').value = '';
+
+    }else if(tipoDeIngresso == "superior" && quantidade <=cadeiraSuperior && quantidade > 0){
+        cadeiraSuperior -=quantidade;
+        cadeiraSuperiorElement.textContent = cadeiraSuperior;
+        document.getElementById('qtd').value = '';
+
+    }else if (tipoDeIngresso == "inferior" && quantidade <=cadeiraInferior && quantidade > 0){
+        cadeiraInferior -=quantidade;
+        cadeiraInferiorElement.textContent = cadeiraInferior;
+        document.getElementById('qtd').value = '';
+    } else{
+        alert('Quantidade de ingressos é superior ao limite! Reveja a quantidade.');
+        document.getElementById('qtd').value = '';
+        return;
+    }
+}
